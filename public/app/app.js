c8789c8bb428f8b61bf2bd25a8caf0e65de18a95
Jongsoo Yoon <jongsoojong@gmail.com>
2016-12-03 20:50:49 -0800

    [progress] Make progress on app.js file, routes mostly complete

--- public/app/app.js
+++ public/app/app.js
@@ -3,11 +3,29 @@
 
   angular
     .module('point-blank', ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-      $stateProvider.state('splash', {
-        url: '/',
-        templateUrl: './splash/splash.template.html',
-        controller: 'splash-controller as vm'
-      });
-    });
+    .config(config);
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider']
+
+      function ($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('splash', {
+          url: '/',
+          templateUrl: './splash/splash.template.html',
+          controller: 'splash-controller as vm'
+        })
+        .state('search', {
+          url: '/search',
+          templateUrl: './search/search.template.html',
+          controller: 'search-controller as vm'
+        })
+        .state('poi', {
+          url: '/poi',
+          templateUrl: './poi/poi.template.html',
+          controller: 'poi-controller as vm'
+        })
+        // add auth when its finalized
+
+      $urlRouterProvider.otherwise('/');
+      }
 })();
