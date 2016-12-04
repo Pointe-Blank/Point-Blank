8c22e951c089ec117b7b211690a20b496e6c0e86
jacuchilla <jcuchilla.csupomona@gmail.com>
2016-12-03 22:08:02 -0800

    [feature] Progress on nav bar directive
    Added section for navigation, including the template, module,
    controller, and directive files. Also added these files
    to the index page with bootstrap to see whether nav bar
    would render.

---
+++ public/app/shared/navigation/navBar.controller.js
@@ -0,0 +1,13 @@
+// (function () {
+//   'use strict';
+
+//   angular
+//         .module('point-blank', [])
+//         .controller('NavBarController', NavBarController);
+
+//   NavBarController.$inject = ['$scope', '$state'];
+
+//   function NavBarController ($scope, $state) {
+//     return 'hello';
+//   }
+// })();
---
+++ public/app/shared/navigation/navBarDirective.js
@@ -0,0 +1,13 @@
+(function () {
+  angular
+        .module('point-blank')
+        // .controller('NavBarController')
+        .directive('navBar', function () {
+          return {
+            restrict: 'E',
+            templateUrl: '/app/shared/navigation/navBar.template.html',
+            replace: true
+          };
+        });
+})()
+;
