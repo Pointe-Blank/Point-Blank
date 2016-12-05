53d42b45c9d2bd316f52822f637b08350d2d3a83
Mark Steadman <r.m.steadman@gmail.com>
2016-12-05 11:43:51 -0800

    [setup] Add base review model

--- server/api/reviews/reviews.model.js
+++ server/api/reviews/reviews.model.js
@@ -1,1 +1,31 @@
 // reviews schema/model
+const Sequelize = require('sequelize');
+const dbConnection = require('../../config/db.config.js');
+
+const Review = dbConnection.define('review', {
+  review_type: {
+    type: Sequelize.STRING
+  },
+  review_content: {
+    type: Sequelize.STRING
+  },
+  rating: {
+    type: Sequelize.INTEGER
+  },
+  created_at: {
+    type: Sequelize.DATE
+  },
+  user_id: {
+    type: Sequelize.INTEGER
+  },
+  poi_id: {
+    type: Sequelize.INTEGER
+  }
+});
+
+// will only create table once; use {force: true} to override table
+Review.sync().then(function () {
+  console.log('Review table successfuly created.');
+});
+
+module.exports = Review;
