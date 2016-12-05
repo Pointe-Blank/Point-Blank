97dad5eaff8c47fdee62cb5de5df23061bb31d4d
Mark Steadman <r.m.steadman@gmail.com>
2016-12-05 11:44:05 -0800

    [setup] Add base user model

--- server/api/users/users.model.js
+++ server/api/users/users.model.js
@@ -1,1 +1,22 @@
 // Users schema/model.
+const Sequelize = require('sequelize');
+const dbConnection = require('../../config/db.config.js');
+
+const User = dbConnection.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
+  credibility_upvotes: {
+    type: Sequelize.INTEGER
+  },
+  credibility_downvotes: {
+    type: Sequelize.INTEGER
+  }
+});
+
+// will only create table once; use {force: true} to override table
+User.sync().then(function () {
+  console.log('User table successfuly created.');
+});
+
+module.exports = User;
