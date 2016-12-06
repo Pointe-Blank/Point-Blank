cd345df13bd6b29bac07555f50fc335411a8c6b7
Jongsoo Yoon <jongsoojong@gmail.com>
2016-12-06 15:27:23 -0800

    [progress] Make progress splash page, trying data

--- public/app/splash/splash.controller.js
+++ public/app/splash/splash.controller.js
@@ -5,9 +5,30 @@
     .module('point-blank.splash')
     .controller('splash-controller', SplashController);
 
-  SplashController.$inject = [];
+  SplashController.$inject = ['splashService'];
 
-  function SplashController () {
+  function SplashController (splashService) {
     var vm = this;
+    vm.poiName = '';
+    vm.poiImage = '';
+    vm.poiSummary = '';
+    vm.posName = '';
+    vm.posImage = '';
+    vm.posSummary = '';
+
+    vm.poiInit = function () {
+      splashService.initPoiSplash()
+        .then(function (result) {
+          vm.poiName = result.name;
+          vm.poiImage = result.profile_image_url;
+          vm.poiSummary = result.summary;
+        });
+      splashService.initPosSplash()
+        .then(function (result) {
+          vm.posName = result.name;
+          vm.posImage = result.profile_image_url;
+          vm.posSummary = result.summary;
+        });
+    };
   }
 })();
