ac04af5e1db7cc40a70db3b2b5cb1015fd17fce6
Mark Steadman <r.m.steadman@gmail.com>
2016-12-06 19:51:14 -0800

    [setup] minimal stuff

--- server/api/reviews/reviews.controllers.js
+++ server/api/reviews/reviews.controllers.js
@@ -1,10 +1,26 @@
 // REMEMBER TO REQUIRE POI MODEL AND DB CONNECTION
+const dbConnection = require('../../config/db.config.js');
 const Review = require('./reviews.model.js');
 
-exports.getAllReviews = function (req, res) {};
+exports.getAllReviews = function (req, res) {
+  Review.findAll()
+    .then(function (review) {
+      res.status(200).json(review);
+    })
+    .catch(function (err) {
+      res.status(400).send(err);
+    });
+};
 
 exports.getOneReviewById = function (req, res) {
   const reviewId = req.params.id;
+  Review.findOne({ where: {name: reviewId} })
+    .then(function (review) {
+      res.status(200).json(review);
+    })
+    .catch(function (err) {
+      res.status(400).send(err);
+    });
 };
 
 exports.addOneReview = function (req, res) {
