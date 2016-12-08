4e5a4196cc7f3f4a5a20e63a45fa0afea55d7e10
jacuchilla <jcuchilla.csupomona@gmail.com>
2016-12-07 18:53:51 -0800

    [progress] Documentation

--- public/app/search/search.controller.js
+++ public/app/search/search.controller.js
@@ -9,8 +9,11 @@
 
   function SearchController ($location, $state, searchService) {
     var vm = this;
+
+    // this variable is being used on the search.template page to load up poi data, the actual variable is declared in the promise from the page initialization
     vm.poiList;
 
+    // this init function is initializing the page on pageload, it invokes a function inside the searchService that will fire off a request to the server for all the poi data
     vm.init = function () {
       searchService.getInitData()
       .then(function (results) {
