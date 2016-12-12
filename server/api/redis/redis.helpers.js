6da9f217f3e1735e47f00ca5bee0f0598e61359a
Khoa Nguyen <knb.nguyen8@gmail.com>
2016-12-12 15:10:43 -0800

    [feature] added guardian news to POI

--- server/api/redis/redis.helpers.js
+++ server/api/redis/redis.helpers.js
@@ -1,7 +1,10 @@
 
 const red_client = require('../../config/redis.config.js');
 const models = require('../../config/db.config.js');
-
+const guardian = require('guardian-js');
+const api = new guardian(process.env.GUARDIAN_API_KEY, false);
+const Promise = require('bluebird');
+const rp = require('request-promise');
 module.exports = {
 
   initUser: () => {
@@ -77,12 +80,84 @@ module.exports = {
     
   },
 
+  initNews: () => {
+
+    return red_client.get('POIs')
+    .then(result=>{
+      //console.log("get POIs", result)
+      if(!result) return []
+      else{
+        let poiList = JSON.parse(result);
+        let arr = []
+
+        let data = poiList.map((poi, index)=>{
+          let options = {
+            uri: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+            qs: {
+              'api-key': process.env.NYTIMES_API_KEY,
+              'q': poi.name
+            },
+            headers: {
+              'User-Agent': 'Request-Promise'
+            },
+            json: true
+          };
+          
+          return Promise.props({
+                  name: poi.name,
+                  summary: poi.summary,
+                  general_rating: poi.general_rating,
+                  personal_rating: poi.personal_rating,
+                  profile_image_url: poi.profile_image_url,
+                  updatedAt: poi.updatedAt,
+                  updatedAt: poi.updatedAt,
+                  Reviews: poi.Reviews,
+                  guardNews:  api.content
+                              .search(poi.name, {
+                                format: 'json',
+                                pageSize: 10
+                              })
+                              .then(result=>{
+                                if(index===0){
+                                  //console.log("look here", JSON.parse(result.body).response.results[0])
+                                }
+                                return JSON.parse(result.body).response.results
+                              }),
+                  // nyNews: Promise.delay(500)
+                  //         .then(()=>{
+                  //           return rp(options)
+                  //         })
+                  //         .then(result => {
+                  //             return result;
+                  //         })
+                  //         .catch(err => {
+                  //           throw err;
+                  //         })
+
+                })
+        })
+
+        Promise.all(data)
+        .then((result)=>{
+          //console.log("Success POIs with news articles", result[0].newsArt);
+          return red_client.set('POIs', JSON.stringify(result))
+        })
+        .then(response=>{
+          console.log("Success POIs with news articles");
+          return response;
+        })
+
+      }
+    })
+    .catch(err=>{
+      console.log("Error intializing Reviews", err)
+    })
+    
+  },
+
   initAll: () => {
 
-    module.exports.initUser()
-    .then(result=>{
-      if(result) return module.exports.initUser()
-    })
+    return module.exports.initUser()
     .then(result=>{
       if(result) return module.exports.initPOI()
     })
@@ -90,13 +165,18 @@ module.exports = {
       if(result) return module.exports.initReview()
     })
     .then(result=>{
-      if(result) console.log("Finished initializing cache")
+      if(result){
+        console.log("Finished initializing cache")
+        return true
+      }else{
+        return false;
+      } 
     })
     .catch(err=>{
       console.log("Error in initAll reddis helpers", err)
     })
     
-  },
+  }
 
  
 }
