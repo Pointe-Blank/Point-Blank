7209d5db5876717da49cee9bc0a23a6e2210c749
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2016-12-12 15:53:18 -0800

    [prog] able to retrieve twitter access token

--- server/api/newsfeed/twitter/twitter.controllers.js
+++ server/api/newsfeed/twitter/twitter.controllers.js
@@ -1,25 +1,46 @@
 const rp = require('request-promise');
 
-const queryTwitter = (req, res) => {
-  let options = {
-    uri: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-    qs: {
-      'api-key': process.env.TWITTER_API_KEY,
-      'q': req.query.querystring
-    },
-    headers: {
-      'User-Agent': 'Request-Promise'
-    },
-    json: true
-  };
+const rfcKey = encodeURI(process.env.TWITTER_CONSUMER_KEY);
+const rfcSecret = encodeURI(process.env.TWITTER_CONSUMER_SECRET);
+const base64btc = Buffer.from(rfcKey + ':' + rfcSecret).toString('base64');
 
-  rp(options)
-    .then(function(returnedTweets) {
-      res.json(returnedTweets);
+let authOptions = {
+  method: 'POST',
+  uri: 'https://api.twitter.com/oauth2/token',
+  headers: {
+    'Authorization': 'Basic ' + base64btc,
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+  },
+  body: 'grant_type=client_credentials'
+};
+
+const queryTwitter = (req, res) => {
+  console.log('Here are our authoptions', authOptions);
+  // let searchOptions = {
+  //   uri: 'https://api.twitter.com/1.1/search/tweets.json',
+  //   qs: {
+  //     'q': req.query.querystring
+  //   },
+  //   headers: {
+  //     'Authorization': 'Request-Promise'
+  //   },
+  //   json: true
+  // };
+
+  rp(authOptions)
+    .then(function(authorization) {
+      console.log('Retrieved authorization token');
+      res.json(JSON.parse(authorization));
     })
+    // .then(function(returnedTweets) {
+    //   console.log(returnedTweets);
+    //   res.json(returnedTweets);
+    // })
     .catch(function(error) {
+      console.log('Error retrieving tweets');
       res.json(error);
     });
+
 };
 
 module.exports = {
